d194b70978051eb2b4ead54f3caf588dc083e3da
masato <masato@m-mbp.local>
2018-10-30 18:00:28 +0800

    Commit

--- Charts_BarChart/ViewController.swift
+++ Charts_BarChart/ViewController.swift
@@ -12,8 +12,9 @@ import Charts
 class ViewController: UIViewController {
 
     var myChartView: BarChartView!
-
     let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +29,8 @@ class ViewController: UIViewController {
 
         // add Graph View as UIView
 
-
-
         let graphView = UIView(frame: CGRect(x: 0, y: self.view.safeAreaInsets.bottom, width: self.view.frame.width, height: self.view.frame.height * 2 / 3))
-
+//
         graphView.backgroundColor = .blue
 
 //        setChart(y: unitsSold)
@@ -43,53 +42,53 @@ class ViewController: UIViewController {
 
     }
 
-//        func setChart(y: [Double]) {
-//            // プロットデータ(y軸)を保持する配列
-//            var dataEntries = [BarChartDataEntry]()
-//
-//            for (i, val) in y.enumerated() {
-//                let dataEntry = BarChartDataEntry(x: Double(i), y: val) // X軸データは、0,1,2,...
-//                dataEntries.append(dataEntry)
-//            }
-//            // グラフをUIViewにセット
-//            let chartDataSet = BarChartDataSet(values: dataEntries, label: "左下に表示されるタイトル")
-//            myChartView.data = BarChartData(dataSet: chartDataSet)
-//
-//            // X軸のラベルを設定
-//            let xaxis = XAxis()
-//
-//            // XAxis()のvalueFormatterに、BarChartFormatter()をセットする
-//            xaxis.valueFormatter = BarChartFormatter()
-//            // グラフが表示されるときに、X軸の1, 2, 3, ... 4が呼び出されるたびに、BarChartFormatter()がX軸のラベルに変換している
-//
-//            myChartView.xAxis.valueFormatter = xaxis.valueFormatter
-//
-//            // x軸のラベルをボトムに表示
-//            myChartView.xAxis.labelPosition = .bottom
-//            // グラフの色
-//            chartDataSet.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
-//            // グラフの背景色
-//            myChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
-//            // グラフの棒をニョキッとアニメーションさせる
-//            myChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
-//            // 横に赤いボーダーラインを描く
-//            let ll = ChartLimitLine(limit: 10.0, label: "Target")
-//            myChartView.rightAxis.addLimitLine(ll)
-//            // グラフのタイトル
-//            myChartView.chartDescription?.text = "Cool Graph!"
-//        }
-//
-//
-//    // NSObjectクラスを継承し、IAxisValueFormatterプロトコルを批准したクラス
-//    public class BarChartFormatter: NSObject, IAxisValueFormatter{
-//        // x軸のラベル
-//        var months: [String]! = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-//
-//        public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-//            // 0 -> Jan, 1 -> Feb...
-//            return months[Int(value)]
-//        }
-//    }
+        func setChart(y: [Double]) {
+            // プロットデータ(y軸)を保持する配列
+            var dataEntries = [BarChartDataEntry]()
+
+            for (i, val) in y.enumerated() {
+                let dataEntry = BarChartDataEntry(x: Double(i), y: val) // X軸データは、0,1,2,...
+                dataEntries.append(dataEntry)
+            }
+            // グラフをUIViewにセット
+            let chartDataSet = BarChartDataSet(values: dataEntries, label: "左下に表示されるタイトル")
+            myChartView.data = BarChartData(dataSet: chartDataSet)
+
+            // X軸のラベルを設定
+            let xaxis = XAxis()
+
+            // XAxis()のvalueFormatterに、BarChartFormatter()をセットする
+            xaxis.valueFormatter = BarChartFormatter()
+            // グラフが表示されるときに、X軸の1, 2, 3, ... 4が呼び出されるたびに、BarChartFormatter()がX軸のラベルに変換している
+
+            myChartView.xAxis.valueFormatter = xaxis.valueFormatter
+
+            // x軸のラベルをボトムに表示
+            myChartView.xAxis.labelPosition = .bottom
+            // グラフの色
+            chartDataSet.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
+            // グラフの背景色
+            myChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
+            // グラフの棒をニョキッとアニメーションさせる
+            myChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+            // 横に赤いボーダーラインを描く
+            let ll = ChartLimitLine(limit: 10.0, label: "Target")
+            myChartView.rightAxis.addLimitLine(ll)
+            // グラフのタイトル
+            myChartView.chartDescription?.text = "Cool Graph!"
+        }
+
+
+    // NSObjectクラスを継承し、IAxisValueFormatterプロトコルを批准したクラス
+    public class BarChartFormatter: NSObject, IAxisValueFormatter{
+        // x軸のラベル
+        var months: [String]! = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+        public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+            // 0 -> Jan, 1 -> Feb...
+            return months[Int(value)]
+        }
+    }
 
 
 }
